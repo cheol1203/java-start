d0b0b552a3e13c891cb57046d7039bf158eb5753
cheol1203 <cheolwon36@naver.com>
2026-01-26 13:25:26 +0900

    7

--- src/variable/Var7.java
+++ src/variable/Var7.java
@@ -2,11 +2,11 @@ package variable;
 
 public class Var7 {
     static void main() {
-        int a = 100;
-        double b = 10.5;
-        boolean c = true;
-        char d = 'A';
-        String e = "Hello Java";
+        int a = 100; //정수
+        double b = 10.5; //실수
+        boolean c = true; // 불리언, treu, false 입력 가능
+        char d = 'A'; //문자 하나
+        String e = "Hello Java"; //문자열, 문자열을 다루기 위한 특별한 타입
 
         System.out.println(a);
         System.out.println(b);
